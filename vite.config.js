2eafa3a64040cd28d9708a6950f45b1449462a77
KevinGrajeda <yokevin20@gmail.com>
2023-03-07 17:01:45 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/display/',
   plugins: [
     react(),
     basicSsl(),
