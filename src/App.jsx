ce8c4fca3cb4134ff26dddccd02fb83e7b6d8237
KevinGrajeda <yokevin20@gmail.com>
2023-03-07 19:25:49 -0600

    estados

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import {
   SliderFilledTrack,
   SliderThumb,
   SliderTrack,
+  Spinner,
   Switch,
+  Text,
   useToast,
   VStack,
 } from '@chakra-ui/react';
@@ -25,9 +27,11 @@ import './styles.css';
 
 function App() {
   const [isConectado, setConectado] = useState(false);
+  const [isProcesando, setProcesando] = useState(false);
   const toast = useToast();
 
   setOnConnect(() => {
+    setProcesando(false);
     setConectado(true);
     toast({
       title: 'display conectado',
@@ -39,6 +43,7 @@ function App() {
   });
 
   setOnDisconnect(() => {
+    setProcesando(false);
     setConectado(false);
     toast({
       title: 'display desconectado',
@@ -50,6 +55,7 @@ function App() {
   });
 
   function handleConexion() {
+    setProcesando(true);
     if (isConectado) {
       disconnectButtonPressed();
     } else {
@@ -58,67 +64,103 @@ function App() {
   }
 
   return (
-    <Box p="10" h="100svh">
+    <Box p="10" pt="0" pb="16" h="100svh">
       <VStack alignItems="start" h="100%">
+        <Box h="10" position="relative" w="100%">
+          {isProcesando && (
+            <Spinner
+              color="yellow.500"
+              size="sm"
+              position="absolute"
+              top="60%"
+              right="0"
+            />
+          )}
+
+          <Box
+            w="2"
+            h="2"
+            bg={isConectado ? 'green.500' : 'red.500'}
+            position="absolute"
+            top="70%"
+            right="1"
+            borderRadius="100%"
+          ></Box>
+        </Box>
         <Heading as="h2" size="xl" mb="10">
           {isConectado ? 'Conectado' : 'Listo para conectar'}
         </Heading>
-        <VStack alignItems="start" w="100%" flex="1" spacing="6">
-          <Box w="100%">
-            <FormLabel htmlFor="texto">Texto</FormLabel>
-            <Input
-              id="texto"
-              onChange={(e) => {
-                sendMessage(e.target.value);
-              }}
-            ></Input>
-          </Box>
-          <HStack>
-            <FormLabel htmlFor="marquee" mb="0">
-              Marquee
-            </FormLabel>
-            <Switch
-              colorScheme="teal"
-              onChange={() => {
-                sendMessage('|Marquee');
-              }}
-              id="marquee"
-            />
-          </HStack>
-          <HStack>
-            <FormLabel htmlFor="invertir" mb="0">
-              Invertir
-            </FormLabel>
-            <Switch
-              colorScheme="teal"
-              onChange={() => {
-                sendMessage('|Invertir');
-              }}
-              id="invertir"
-            />
-          </HStack>
-          <Box w="100%">
-            <FormLabel htmlFor="brillo">Brillo</FormLabel>
-            <Slider
-              colorScheme="teal"
-              id="brillo"
-              aria-label="brillo"
-              defaultValue={0}
-              min={0}
-              max={15}
-              size="lg"
-              onChangeEnd={(value) => {
-                console.log(`|Brillo|${value}`);
-                sendMessage(`|Brillo|${value}`);
-              }}
-            >
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb boxSize="6" />
-            </Slider>
-          </Box>
-        </VStack>
+        {!isConectado && (
+          <>
+            <Text flex="1" pt="4">
+              Para conectarte al esp-32 da clic en el boton
+            </Text>
+          </>
+        )}
+        {isConectado && (
+          <VStack alignItems="start" w="100%" flex="1" spacing="6">
+            <Box w="100%">
+              <FormLabel htmlFor="texto">Texto</FormLabel>
+              <Input
+                id="texto"
+                disabled={!isConectado}
+                onChange={(e) => {
+                  sendMessage(e.target.value);
+                }}
+              ></Input>
+            </Box>
+            <HStack>
+              <Switch
+                disabled={!isConectado}
+                colorScheme="teal"
+                size="lg"
+                onChange={() => {
+                  sendMessage('|Marquee');
+                }}
+                id="marquee"
+              />
+              <FormLabel htmlFor="marquee" mb="0">
+                Marquee
+              </FormLabel>
+            </HStack>
+            <HStack>
+              <Switch
+                disabled={!isConectado}
+                colorScheme="teal"
+                size="lg"
+                onChange={() => {
+                  sendMessage('|Invertir');
+                }}
+                id="invertir"
+              />
+              <FormLabel htmlFor="invertir" mb="0">
+                Invertir
+              </FormLabel>
+            </HStack>
+            <Box w="100%">
+              <FormLabel htmlFor="brillo">Brillo</FormLabel>
+              <Slider
+                disabled={!isConectado}
+                colorScheme="teal"
+                id="brillo"
+                aria-label="brillo"
+                defaultValue={0}
+                min={0}
+                max={15}
+                size="lg"
+                onChangeEnd={(value) => {
+                  console.log(`|Brillo|${value}`);
+                  sendMessage(`|Brillo|${value}`);
+                }}
+              >
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb boxSize="6" />
+              </Slider>
+            </Box>
+          </VStack>
+        )}
         <Button size="lg" w="100%" colorScheme="teal" onClick={handleConexion}>
           {isConectado ? 'desconectar' : 'conectar'}
         </Button>
